15663dbd9702dfe7a8a1d3e74117c943b6aae392
Kyle Richardson <kyle.richardson1@gmail.com>
2020-08-25 22:41:30 -0500

    removed warnings

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Main from './components/Main'
 import Header from './components/Header'
 import Footer from "./components/Footer"
@@ -6,10 +6,6 @@ import { useDarkMode } from "./utils/useDarkMode"
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 
 function App() {
-  // const [darkMode, setDarkMode] = useDarkMode()
-  // const toggleDarkMode = () => {
-  //   setDarkMode(!darkMode)
-  // }
 
   const [darkMode, setDarkMode] = useDarkMode();
   const palletType = darkMode ? "dark" : "light";
